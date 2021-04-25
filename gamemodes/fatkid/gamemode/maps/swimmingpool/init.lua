4d171e0750283bbf658eecaa32ecf4a15c02ac70
Swamp <swampservers@gmail.com>
2021-04-25 16:03:45 -0500

    Tweaks for new map and fix other map

--- gamemodes/fatkid/gamemode/maps/swimmingpool/init.lua
+++ gamemodes/fatkid/gamemode/maps/swimmingpool/init.lua
@@ -1,10 +1,5 @@
 ﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
 -- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
-GM.AZ.StartSpeed = 60
-GM.AZ.EndSpeed = 120
---GM.Zombie.StartSpeed = 450
---GM.Zombie.EndSpeed = 525
-GM.AZ.JumpPower = 200
-GM.Zombie.JumpPower = 280
 
-GM.BarricadeBaseHealth = 1400
+
+GM.BarricadeBaseHealth = 600
