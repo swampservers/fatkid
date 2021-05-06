a13193eab7a89329261cfa9c77e61343315db9ef
CodyThe <81999636+CodyThe@users.noreply.github.com>
2021-05-05 22:11:54 -0400

    adding backstory.
    REEEEEEEEEEEEEEE

---
+++ gamemodes/fatkid/gamemode/maps/futuristicsubmarine/sh_init.lua
@@ -0,0 +1,13 @@
+  
+-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+
+FATKID_BACKSTORY = [[
+<p>
+It was just an ordinary field trip in a nuclear submarine.
+Gregory, the class fat kid, was being bullied for his lacking mental capacity.
+But then, after being hit with a particularly heavy aluminium pan, something inside his brain changed.
+Or perhaps it was just hunger, as his fat ass hadn't eaten in over three hours.
+Either way, he started eating his fellow classmates in a murderous rampage of gluttony...
+</p>
+]]
