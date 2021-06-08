3de89d327bb7588287e5464f0771ba7444e6b535
goldenmagicboy <69483457+goldenmagicboy@users.noreply.github.com>
2021-06-08 13:13:10 -0500

    Storyline Added*
    "It was a special day for Gregory's class. The teachers scheduled a field trip to the mines.
    each student was allowed to bring one snack. Which wasn't enough for Gregory ofcourse. while the class
    were waiting for their Tour guide at the mines. Some kids were gossiping about Gregory behind his back.
    One told the shy kid to get the biggest rock you can find and chuck it at Gregory's ass. The shy kid
    got a pretty dull rock and threw it at Gregory. At that very moment,  something inside his fragile autistic mind snapped.
    Or perhaps it was just hunger, as his fat ass hadn't eaten a complete meal in 3 hours.
    Either way, he deepthroated the shy kid, and started eating his fellow classmates in a murderous rampage of gluttony..."

---
+++ gamemodes/fatkid/gamemode/maps/sh_init.lua
@@ -0,0 +1,13 @@
+-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+FATKID_BACKSTORY = [[
+<p>
+It was a special day for Gregory's class. The teachers scheduled a field trip to the mines.
+each student was allowed to bring one snack. Which wasn't enough for Gregory ofcourse. while the class
+were waiting for their Tour guide at the mines. Some kids were gossiping about Gregory behind his back.
+One told the shy kid to get the biggest rock you can find and chuck it at Gregory's ass. The shy kid
+got a pretty dull rock and threw it at Gregory. At that very moment,  something inside his fragile autistic mind snapped.
+Or perhaps it was just hunger, as his fat ass hadn't eaten a complete meal in 3 hours.
+Either way, he deepthroated the shy kid, and started eating his fellow classmates in a murderous rampage of gluttony...
+</p>
+]]
