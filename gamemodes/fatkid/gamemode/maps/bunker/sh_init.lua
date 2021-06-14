992ee9f617eff543d960e21fa0b4890f71765247
alao3alao3 <aidanlao3@gmail.com>
2021-06-14 15:28:47 -0400

    so like do these files get called somewhere

---
+++ gamemodes/fatkid/gamemode/maps/bunker/sh_init.lua
@@ -0,0 +1,9 @@
+﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+FATKID_BACKSTORY = [[
+<p>
+The whole class has been invited to a housewarming party in celebration of the purchase of a faraway ranch house by a fellow classmate. 
+The whole class, except Gregory, of course. The barring of entry to a catered event however is a mortal sin in the eyes of any fatass and not taken kindly.
+Not one to leave empty handed, Gregory descends into a voracious frenzy, forcing the atendees to flee into the nearby bunker the seller explicitly stated should not be entered under any circumstance.
+</p>
+]]
