a16c77608d5a8fcbcd6114a44ec7b94a5c96c827
CodyThe <81999636+CodyThe@users.noreply.github.com>
2021-08-09 19:07:52 -0400

    adding the folder and sh_unit.lua

---
+++ gamemodes/fatkid/gamemode/maps/submarine/sh_init.lua
@@ -0,0 +1,13 @@
+-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+
+FATKID_BACKSTORY = [[
+<p>
+It was just a local field trip in the nuclear submarine.
+Gregory, the class fat kid, was being bullied for shoving My Little Pony figurines up his nose.
+But then, after pushing a figurine a little too far up his nose,
+Or it was his fatass not having eaten in an hour.
+Either way, his autistic mind didn't handle too well, and he started eating his classmates...
+Can you escape his rampage?
+</p>
+]]
