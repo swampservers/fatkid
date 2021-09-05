b3e973fe9c570661f6dbbd4648bbd69a9d1fd41b
DaFloppa <85963424+DaFloppa@users.noreply.github.com>
2021-09-05 17:52:56 -0500

    Update sh_init.lua

--- gamemodes/fatkid/gamemode/maps/catacombs/sh_init.lua
+++ gamemodes/fatkid/gamemode/maps/catacombs/sh_init.lua
@@ -1,11 +1,12 @@
 -- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
 -- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
 FATKID_BACKSTORY = [[
-<h2>Welcome to Fattywood Medical Center.</h2>
+<h2>Welcome to the Underground, how was the fall.</h2>
 <p>
-It was a normal day for Gregory out in the playground... until...
-The ground started rumbling... it started cracking open... and everyone fell in! His fatass made an earthquake just by walking!
-The fatkid wanted to be nice and try to help everyone out, but the other kids didn't want to hear it. Now they will have
-to traverse the underground "catacombs" while running away from the fatkid after being so mean to him.
+Gregory and the skinnies after school like to play tag, usually ending up with Gregory
+being it for the whole game since he's so fat. But one day... they were playing around
+Mt. Ebott, where a mysterious and seeminly abandoned mining hole was left, but the Fat
+Kid fell in! The skinny kids tried to help him out by creating a human rope, but they
+all fell in too. Gregory got so mad, he went on yet another rampage through these catacombs.
 </p>
 ]]
