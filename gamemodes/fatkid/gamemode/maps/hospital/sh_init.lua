0fe0da213567edc35748db17ce3ad7d33515bd1d
alao3alao3 <aidanlao3@gmail.com>
2022-01-21 15:46:17 -0500

    stripmall

--- gamemodes/fatkid/gamemode/maps/hospital/sh_init.lua
+++ gamemodes/fatkid/gamemode/maps/hospital/sh_init.lua
@@ -7,7 +7,7 @@ It was a normal school day for Gregory, getting dodgeballs thrown at him, and ge
 Until... someone threw a dodgeball at his fat stomach and burst his appendix! He had to get to a hospital immediately!
 The closest hospital was sadly far away in City 17. Right before his surgery, the skinny kids wanted to come by to visit.
 But once Gregory found out about their arrival, HE GOT MAD! He almost ate his doctors!
-The entire hospital faculty got scared and fleed, leaving behind barries so that the fat kid couldn't get them.
+The entire hospital faculty got scared and fleed, leaving behind barriers so that the fat kid couldn't get them.
 Now the barriers would just be a problem for the skinny kids to survive...
 It's a good thing the Donald Trump approved "Arm our Hospitals" bill was just passed, or else the skinny kids wouldnt stand a chance.
 </p>
---
+++ gamemodes/fatkid/gamemode/maps/stripmall/sh_init.lua
@@ -0,0 +1,11 @@
+﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+FATKID_BACKSTORY = [[
+<h2>Welcome to Fattywood Mall.</h2>
+<p>
+School is out, and the class has decided to hang out at the local stripmall as they have so many times before.
+Due to the rise in smash-and-grab crimes however, a majority of the businesses have been forced to close, barricade, and arm themselves, much to the dismay of the classmates.
+This is especially hard on Gregory, who's just found out his favorite Malaysian restaurant at the mall has closed it's doors for rennovations.
+Angered and starving, he leaps out of his mother's minivan in a huff and slips into yet another one of his autistic rages.
+</p>
+]]
