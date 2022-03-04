39c354605e96219e92e96048248ef8c26b7857b9
CodyThee <100976773+CodyThee@users.noreply.github.com>
2022-03-04 18:55:42 -0500

    Backstory + barricade health

---
+++ gamemodes/fatkid/gamemode/maps/quarry/sh_unit.lua
@@ -0,0 +1,10 @@
+-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+FATKID_BACKSTORY = [[
+<h2>Welcome to Fattywood Meth-Mining Quarry.</h2>
+<p>
+'Twas upon a normal field trip Gregory, the local fatass at the retard school, started to inhale the methamphetamine coming from the quarry during the field trip.
+After he was ticked he started eating his classmates, the teacher, the bus they came in on, and started to want more.
+It is your job, the skinny kids to live until he dies due to his blood-sugar levels being too high.
+</p>
+]]
