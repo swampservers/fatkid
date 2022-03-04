b7c1ca8860caad3f860b2455387047d48cfeb0de
alao3alao3 <62276730+alao3alao3@users.noreply.github.com>
2022-03-04 01:24:00 -0500

    the hell do you write for pitbase lore? fieldtrip?

--- gamemodes/fatkid/gamemode/maps/pitbase/sh_init.lua
+++ gamemodes/fatkid/gamemode/maps/pitbase/sh_init.lua
@@ -1,10 +1,7 @@
 ﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
 -- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
 FATKID_BACKSTORY = [[
-<h2>Welcome to Fattywood Community Center.</h2>
+<h2>Welcome to PitBase.</h2>
 <p>
-During summer break, Gregory's mom drops him off at the pool to try and get him some exercise.
-As soon as he stepped onto the diving platform, he's in plain view of the patrons at the pool, immediately resulting in mockery and namecalling.
-As usual, .he fat boy's temper boils over and he lurches into a gluttonous rage, eating everyone in sight...
 </p>
 ]]
