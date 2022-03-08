14b69353c1d8dfacf1a097b5270a6c988dc15c99
alao3alao3 <aidanlao3@gmail.com>
2022-03-07 23:24:25 -0500

    Typo

--- gamemodes/fatkid/gamemode/maps/asylum/sh_init.lua
+++ gamemodes/fatkid/gamemode/maps/asylum/sh_init.lua
@@ -5,7 +5,7 @@ FATKID_BACKSTORY = [[
 <p>
 In the evening, the skinny kids while on their trip to the way back home, suddenly ran out of gasoline. 
 As night fell, the skinny kids got out of their seats in search for gasoline, and saw a distant gateway to the Asylum "for the Fatty"
-They decided to go through asylum in search of the gasoline until, out of nowhere, fatkid shows up and blocks the road back bus.
+They decided to go through asylum in search of the gasoline until, out of nowhere, fatkid shows up and blocks the road back to the bus.
 Will the skinny kids survive this asylum?
 </p>
 ]]
