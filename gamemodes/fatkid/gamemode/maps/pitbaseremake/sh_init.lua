5980dddcd297d11e0e429cfd03ab1be8c0e71bfb
alao3alao3 <aidanlao3@gmail.com>
2022-03-21 00:02:39 -0400

    init commit

---
+++ gamemodes/fatkid/gamemode/maps/pitbaseremake/sh_init.lua
@@ -0,0 +1,7 @@
+﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+FATKID_BACKSTORY = [[
+<h2>Welcome to Pit Base (Remake).</h2>
+<p>
+</p>
+]]
