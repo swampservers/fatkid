e9e842fa1f04a09aaee57f4cc8a4b11dd5eb7e2e
CodyThee <100976773+CodyThee@users.noreply.github.com>
2022-04-10 14:17:39 -0400

    Made it harder
    Made the map a bit harder for the skinny kids, as they would just demolish the skeletons late game.

--- gamemodes/fatkid/gamemode/maps/quarry/init.lua
+++ gamemodes/fatkid/gamemode/maps/quarry/init.lua
@@ -1,3 +1,7 @@
 ﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
 -- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
-GM.BarricadeBaseHealth = 1100
+GM.BarricadeBaseHealth = 1300
+GM.Zombie.StartSpeed = 500
+GM.Zombie.EndSpeed = 550
+GM.AZ.StartSpeed = 65
+GM.AZ.EndSpeed = 110
