524ba4ecec5ab234d2e593561dbc83e5e421ffe4
alao3alao3 <62276730+alao3alao3@users.noreply.github.com>
2023-10-11 18:41:30 -0400

    New Map, Fatkid_Mansion (#56)

---
+++ gamemodes/fatkid/gamemode/maps/mansion/sh_unit.lua
@@ -0,0 +1,11 @@
+﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+-- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
+FATKID_BACKSTORY = [[
+<h2>Welcome to Fattywood Estate.</h2>
+<p>
+They say, on a night like this, an ugly old hag can be seen wandering the halls of the old run-down mansion north of town.
+On a double-dog-dare, the Skinny Kids have taken their school bus to see if the rumors are true.
+Gregory however overhead their plans and is furious at their remarks towards both his grandmother and her house.
+Seeking to defend his family from ridicule, Gregory ambushes the Skinny Kids, forcing them deeper into the estate...
+</p>
+]]
