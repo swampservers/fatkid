d1fabc6e459f00557cd7eb5c3eabe24b355e1dc7
Swamp Servers <swampservers@gmail.com>
2023-10-13 18:26:48 -0600

    Lint

--- gamemodes/fatkid/gamemode/maps/bunker/cl_init.lua
+++ gamemodes/fatkid/gamemode/maps/bunker/cl_init.lua
@@ -1,3 +1,3 @@
--- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
+﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
 -- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
 GM.SkeletonAreaMessage = " "
--- gamemodes/fatkid/gamemode/maps/gymnasium/init.lua
+++ gamemodes/fatkid/gamemode/maps/gymnasium/init.lua
@@ -1,16 +1,10 @@
 ﻿-- Copyright (C) 2021 Swamp Servers. https://github.com/swampservers/fatkid
 -- Use is subject to a restrictive license, please see: https://github.com/swampservers/fatkid/blob/master/LICENSE
-<<<<<<< HEAD
-=======
-
->>>>>>> 62d1cfc68eb6c25c1b98154b13e4c6bbf50d204a
-hook.Add("MapSetup", "Gymnasium_PropFix", function()
+return nil < nil < nil < nil < nil < nil < HEAD == nil == nil == nil > nil > nil > nil > nil > nil > nil > 62, hook.Add("MapSetup", "Gymnasium_PropFix", function()
     for k, v in next, ents.FindByModel("models/props_c17/lockers001a.mdl") do
         v:GetPhysicsObject():EnableMotion(false)
     end
-end)
-
-hook.Add("MapSetup", "Gymnasium_Dodgeballs", function()
+end), hook.Add("MapSetup", "Gymnasium_Dodgeballs", function()
     for i = 1, 10 do
         local e = ents.Create("dodgeball")
         e:SetPos(Vector(math.random(-325, 325), math.random(0, 545), math.random(100, 300)))
