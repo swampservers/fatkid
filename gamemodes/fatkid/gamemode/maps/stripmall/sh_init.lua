064944387ea878c5f2a7dd974c1a779ecc51331c
alao3alao3 <62276730+alao3alao3@users.noreply.github.com>
2024-04-29 23:49:12 -0400

    Rewords sh_init.lua

--- gamemodes/fatkid/gamemode/maps/stripmall/sh_init.lua
+++ gamemodes/fatkid/gamemode/maps/stripmall/sh_init.lua
@@ -4,8 +4,8 @@ FATKID_BACKSTORY = [[
 <h2>Welcome to Fattywood Mall.</h2>
 <p>
 School is out, and the class has decided to hang out at the local stripmall as they have so many times before.
-Due to the rise in smash-and-grab crimes however, a majority of the businesses have been forced to close, barricade, and arm themselves, much to the dismay of the classmates.
-This is especially hard on Gregory, who's just found out his favorite Malaysian restaurant at the mall has closed it's doors for rennovations.
+Due to a rise in crime however, a majority of the businesses have been forced to close, barricade, and arm themselves, much to the dismay of the students.
+This is especially hard on Gregory, who's just found out his favorite restaurant at the stripmall has closed it's doors for rennovations.
 Angered and starving, he leaps out of his mother's minivan in a huff and slips into yet another one of his autistic rages.
 </p>
 ]]
